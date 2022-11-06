b8c066bfe1d55ae848e463d6d10c53989e146699
Nikunj Kumar <nikunjkumar1988@gmail.com>
2022-11-06 16:11:26 +0530

    First Commit - 6-nov-2022

---
+++ Class_1/input_output.py
@@ -0,0 +1,13 @@
+# Declare & assign variables
+
+int_var = 10
+float_var = 18.25
+string_var = "nikunj"
+
+bool_var = True
+
+#Function in python to print output
+print(int_var)
+print(float_var)
+print(string_var)
+print(bool_var)
